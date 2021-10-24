cf32c3959f0aca40e043d40d737c897593213b92
fabrifj <fabrifj@gmail.com>
2021-10-24 10:39:49 -0400

    update

--- PizzaBrosFE/src/app/app.module.ts
+++ PizzaBrosFE/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './modules/home/home.component';
 import { ActiveOrdersComponent } from './modules/orders/active-orders/active-orders.component';
 import { ClientDataComponent } from './modules/orders/client-data/client-data.component';
-import { DoOrderComponent } from './modules/orders/do-order/do-order.component';
+import { DoOrderComponent } from './modules/do-order/do-order.component';
 import { OrdersHistoryComponent } from './modules/orders/orders-history/orders-history.component';
 import { OrdersComponent } from './modules/orders/orders.component';
 import { PBRoutersModule } from './pb-router/PB-routers.module';
+import { ListSubMenuComponent } from './shared-modules/list-sub-menu/list-sub-menu.component';
+import { ComSubMenuComponent } from './shared-modules/list-sub-menu/com-sub-menu/com-sub-menu.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,10 @@ import { PBRoutersModule } from './pb-router/PB-routers.module';
     ActiveOrdersComponent,
     ClientDataComponent,
     DoOrderComponent,
-    OrdersHistoryComponent
+    OrdersHistoryComponent,
+    ListSubMenuComponent,
+    ComSubMenuComponent,
+    
 
 
   ],
--- PizzaBrosFE/src/app/pb-router/PB-routers.module.ts
+++ PizzaBrosFE/src/app/pb-router/PB-routers.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../modules/home/home.component';
 import { ActiveOrdersComponent } from '../modules/orders/active-orders/active-orders.component';
-import { DoOrderComponent } from '../modules/orders/do-order/do-order.component';
+import { DoOrderComponent } from '../modules/do-order/do-order.component';
 import { OrdersHistoryComponent } from '../modules/orders/orders-history/orders-history.component';
 import { OrdersComponent } from '../modules/orders/orders.component';
 
