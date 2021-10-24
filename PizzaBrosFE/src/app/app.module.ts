d87a47cf7eef3b66cbb65f6e6ee914ffdc0a32b1
Andrea Sarmiento <anbesamo@gmail.com>
2021-10-24 00:52:40 -0400

    PBF-15 added part of modal component

--- PizzaBrosFE/src/app/app.module.ts
+++ PizzaBrosFE/src/app/app.module.ts
@@ -12,6 +12,9 @@ import { OrdersComponent } from './modules/orders/orders.component';
 import { PBRoutersModule } from './pb-router/PB-routers.module';
 import { MainButtonsComponent } from './modules/orders/main-buttons/main-buttons.component';
 
+import { ListObjectComponent } from './modules/orders/list-object/list-object.component';
+import { ModalModule } from './modules/orders/modal/modal.module';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,13 +25,15 @@ import { MainButtonsComponent } from './modules/orders/main-buttons/main-buttons
     ClientDataComponent,
     DoOrderComponent,
     OrdersHistoryComponent,
-    MainButtonsComponent
+    MainButtonsComponent,
+    ListObjectComponent
 
 
   ],
   imports: [
     BrowserModule,
-    PBRoutersModule
+    PBRoutersModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
