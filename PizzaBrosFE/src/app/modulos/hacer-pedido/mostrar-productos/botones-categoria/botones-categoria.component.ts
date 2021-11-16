275a7eb170beb28763b18b80ddcd3a7cb782afe0
Jud4 <Jud4@users.noreply.github.com>
2021-11-16 14:58:53 -0400

    PFI-12 Creacion componente botones categoria

---
+++ PizzaBrosFE/src/app/modulos/hacer-pedido/mostrar-productos/botones-categoria/botones-categoria.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MostrarProductosComponent} from "../mostrar-productos.component";
+
+@Component({
+  selector: 'app-botones-categoria',
+  templateUrl: './botones-categoria.component.html',
+  styleUrls: ['./botones-categoria.component.css']
+})
+export class BotonesCategoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
