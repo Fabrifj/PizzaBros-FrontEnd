41311bfa955f53ea88f3ac1568f9aab73ee1d3ff
Jud4 <Jud4@users.noreply.github.com>
2021-11-15 20:34:18 -0400

    PFI-12 Creación componente

---
+++ PizzaBrosFE/src/app/modulos/hacer-pedido/mostrar-productos-categoria/mostrar-productos-categoria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-productos-categoria',
+  templateUrl: './mostrar-productos-categoria.component.html',
+  styleUrls: ['./mostrar-productos-categoria.component.css']
+})
+export class MostrarProductosCategoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
