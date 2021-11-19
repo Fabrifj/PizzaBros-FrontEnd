3e24da6624b34c05200aa5e2523e94ba59acead6
Boris Pedraza <bpedrazaa@gmail.com>
2021-11-18 20:19:38 -0400

    BB-DivisionColecciones Refactorizacion de coleccion Compra implementada.

--- YourPizzaBros3/index.js
+++ YourPizzaBros3/index.js
@@ -15,6 +15,7 @@ const fnProducto = require('./producto');
 const fnCategoria = require('./categoria');
 const fnPedido = require('./pedido');
 const fnElemento = require('./elemento');
+const fnCompra = require('./compra');
 
 /*===================================
           CRUD PRODUCTOS
@@ -239,237 +240,39 @@ app.put("/api/elemento/:id/agregarInv", async (req, res) => {
           CRUD COMPRA
 ===================================*/
 
-/* 
-Estructura del body para la creacion de una compra
-{
-    "Fecha":"2021-02-14T",
-    "Total":60,
-    "ListaElementos":
-    [
-        {
-            "IdElemento":"1",
-            "Marca":"Salsa de Tomate Flor",
-            "TipoUnidad":"ml",
-            "CantidadMedida":500,
-            "CostoUnidad":15,
-            "CantidadComprada":2,
-            "Tipo": "Ingrediente",
-            "Nombre": "Salsa de Tomate"
-        },
-        {
-            "IdElemento":"2",
-            "Marca":"Harina BlancaFlor",
-            "TipoUnidad":"kg",
-            "CantidadMedida":0.5,
-            "CostoUnidad":10,
-            "CantidadComprada":3,
-            "Tipo": "Ingrediente",
-            "Nombre:" "Salsa de Tomate"
-        }
-    ]
-}
-*/
-
 // Crear una compra
-async function CrearElem(elem){
-  await elemento.add(elem);
-} 
-
 app.post("/api/compra", async (req, res) => {
   const data = req.body;
-
-  const fecha = data.Fecha;
-  const total = data.Total;
-
-  var misElems = [];
-  var msg = {};
-  // Iteramos por todos los elementos dentro de la lista de elementos
-  data.ListaElementos.forEach(
-    async (elem) => {
-      //Creacion del objeto elemento necesario para la creacion de la base de datos
-      var elemBD = {
-        "IdElemento": elem.IdElemento,
-        "Marca": elem.Marca,
-        "TipoUnidad": elem.TipoUnidad,
-        "CantidadMedida": elem.CantidadMedida,
-        "CostoUnidad": elem.CostoUnidad,
-        "CantidadComprada": elem.CantidadComprada
-      }
-      misElems.push(elemBD);
-      //console.log("Elementos a ingresar dentro de compra en la BD: ", misElems);
-
-      // Creacion de la compra individual
-      var nuevaCompraIndividual = {
-        "Tipo":"Individual",
-        "Fecha": firebase.firestore.Timestamp.fromDate(new Date(fecha)),
-        "Total": total,
-        "CostoUnidad": elem.CostoUnidad,
-        "CantidadComprada": elem.CantidadComprada,
-        "TipoUnidad": elem.TipoUnidad,
-        "CantidadMedida": elem.CantidadMedida,
-        "IdElemento": elem.IdElemento
-      }
-
-      console.log("Nueva compra individual a ingresar en la base de datos: ", nuevaCompraIndividual);
-      await compra.add(nuevaCompraIndividual);
-      console.log("Compra individual agregada");
-      msg["Compra Individual"] = nuevaCompraIndividual;
-
-
-      // Verificacion de la existencia del elemento dentro de la base de datos
-      await elemento.doc(elem.IdElemento).get().then(snapshot => {
-        let querySnapshot = snapshot.data();
-        //console.log("Resultado de la consulta: ", querySnapshot);
-          
-        if (typeof querySnapshot == 'undefined' || querySnapshot.empty || querySnapshot == null){
-          console.log(`elemento con el id: ${elem.IdElemento} no encontrado en la base de datos`);
-          // formato del elemento
-          var cantInventario = elem.CantidadMedida * elem.CantidadComprada;
-          var miElem = {
-            "Nombre": elem.Nombre,
-            "Tipounidad": elem.TipoUnidad,
-            "ListaArticulos":
-            [
-                {
-                    "Marca": elem.Marca,
-                    "Costo": elem.CostoUnidad,
-                    "CantidadMedida": elem.CantidadMedida
-                }
-            ],
-            "CantidadInventario": cantInventario,
-            "CostoMedia": elem.CostoUnidad,
-            "Tipo": elem.Tipo
-          }
-          //console.log("En caso de que el elemento no este creado: ", miElem);
-
-          // Creacion del elemento con la funcion de Gaby
-          
-          CrearElem(miElem);
-        } else {
-          // Dado que el elemento existe se capturara los nuevos datos que el elemento
-          // trae con la compra
-          console.log("Elemento encontrado: ", elem.IdElemento);
-
-          var miElem = {
-            "Marca": elem.Marca,
-            "CantidadMedida": elem.CantidadMedida,
-            "Costo": elem.CostoUnidad,
-            "CantidadComprada": elem.CantidadComprada      
-          }
-          //console.log("En caso de que el elemento este creado: ", miElem);
-
-          // Actualizacion del elemento mediante la funcion de Gaby
-          actualizarElemAgregarInv(elem.IdElemento,miElem);
-
-        }
-      })
-    }
-  )
-  var respuesta = null;
-
-  // Creacion de la compra de tipo grupal
-  var nuevaCompraGrupal = {
-    "Tipo": "Grupal",
-    "Fecha": firebase.firestore.Timestamp.fromDate(new Date(fecha)),
-    "Total": total,
-    "ListaElementos": misElems
-  }
-  console.log("Nueva compra grupal a ingresar en la base de datos: ", nuevaCompraGrupal);
-  await compra.add(nuevaCompraGrupal);
-  console.log("Compra grupal agregada");
-  msg["CompraGrupal"] = nuevaCompraGrupal;
-
-  respuesta = msg;
+  const respuesta = await fnCompra.crearCompra(data)
   res.send(respuesta);
 })
 
-
-//Obtener todas las compras
-async function obtenerCompras(){
-  const snapshot = await compra.get();
-  const list = snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data()
-  }));
-  return list;
-}
-
+//ObtenerCompras
 app.get("/api/compras", async (req, res) => {
-  const lista = await obtenerCompras();
+  const lista = await fnCompra.obtenerCompras();
   res.send(lista);
 })
 
-// Obtener compra mediante su ID
-async function obetenerCompraId(compraId) 
-{
-  var resp = null;
-  await compra.doc(compraId).get().then((doc) => {
-    if (doc.exists) {
-      resp = { id: doc.id, ...doc.data() }  
-      console.log("Informacion de la compra:", doc.data());
-    } else {
-      console.log("La compra no existe");
-    }
-  }).catch((error) => 
-  {
-      console.log("Error getting document:", error);
-  });
-
-  return resp;
-}
-
+//ObtenerCompraId
 app.get("/api/compra/:id", async (req, res) => {
   var compraId = req.params.id;
-  const resp = await obetenerCompraId(compraId);
-  res.send(resp);
+  const respuesta = await fnCompra.obtenerCompraId(compraId);
+  res.send(respuesta);
 });
 
-
-// Actualizar compra
-async function actualizarCompra(compraId, cmp) {
-  
-  var resp = null;
-  await compra.doc(compraId).update(cmp)
-  .then(() => 
-  {
-    resp = cmp;  
-    console.log("Compra actualizada");
-  })
-  .catch((error) => 
-  {
-      // The document probably doesn't exist.
-      console.error("Error al actualizar la compra: ", error);
-  });
-
-  return resp;
-};
-
+//ActualizarCompra
 app.put("/api/compra/:id", async (req, res) => {
   var compraId = req.params.id;
   var cmp = req.body;
-  const resp = await actualizarCompra(compraId,cmp);
-  res.send(resp);
+  const respuesta = await fnCompra.actualizarCompra(compraId,cmp);
+  res.send(respuesta);
 });
 
-
-// Eliminar Compra
-async function eliminarCompra(compraId) {
-  var resp = null;
-  await compra.doc(compraId).delete().then(() => {
-    resp = "Compra borrada correctamente!"
-    console.log(resp);
-  }).catch((error) => 
-  {
-    console.error("Error eliminando la compra: ", error);
-  });
-
-  return resp;
-}
-
+//EliminarCompra
 app.delete("/api/compra/:id", async (req, res) => {
   var compraId = req.params.id;
-  const resp = await eliminarCompra(compraId);
-  res.send(resp);
+  const respuesta = await fnCompra.eliminarCompra(compraId);
+  res.send(respuesta);
 });
 
 ///////
