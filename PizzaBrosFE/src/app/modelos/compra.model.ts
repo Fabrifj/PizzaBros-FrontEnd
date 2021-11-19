ac0afe580be7186da889b6c4fc0e9978d6a7cc68
fabrifj <fabrifj@gmail.com>
2021-11-19 16:25:48 -0400

    updated

--- PizzaBrosFE/src/app/modelos/compra.model.ts
+++ PizzaBrosFE/src/app/modelos/compra.model.ts
@@ -1,11 +1,13 @@
+import { ArticuloModel } from "./articulo.model";
+
 export class ingredienteModel {
     public Id: number;
     public Fecha: string;
     public Total: number;
-    public ListaIngredientes: ingredienteModel[];
+    public ListaIngredientes: ArticuloModel[];
 
   
-    constructor(id: number, fecha:string,total:number, ListaIngredientes: ingredienteModel[]) {
+    constructor(id: number, fecha:string,total:number, ListaIngredientes: ArticuloModel[]) {
       this.Id = id;
       this.Fecha = fecha;
       this.Total = total;
--- PizzaBrosFE/src/app/modulos/empleados/detalles-empleados/detalles-empleados.component.ts
+++ PizzaBrosFE/src/app/modulos/empleados/detalles-empleados/detalles-empleados.component.ts
@@ -9,7 +9,21 @@ export class DetallesEmpleadosComponent implements OnInit {
 
   constructor() { }
 
+  datos: any | undefined;
+
+  columnas = [
+    {field:'NITCliente',header:'NIT de Cliente'},
+    {field:'NombreCliente',header:'Nombre de Cliente'},
+    {field:'Precio',header:'Total Bs.'},
+    {field:'Fecha',header:'Fecha'},
+    {field:'Estado',header:'Estado'}
+  ];
+  nombreBotones: string[] | undefined;
+
   ngOnInit(): void {
+  }
+  funcionBoton( nombres: any){
+
   }
 
 }
