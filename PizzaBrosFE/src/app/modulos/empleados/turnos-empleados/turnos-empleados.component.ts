b4934e588efb0d42e465b28884c199d18c0ef771
Stephany78315 <stephanyduran78315@gmail.com>
2021-11-27 19:29:05 -0400

    Añadiendo la estructura de turnos empleados

--- PizzaBrosFE/src/app/modulos/empleados/turnos-empleados/turnos-empleados.component.ts
+++ PizzaBrosFE/src/app/modulos/empleados/turnos-empleados/turnos-empleados.component.ts
@@ -1,4 +1,7 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { AppHttpService } from 'src/app/servicios/app-http.service';
+import { ModalService } from 'src/app/shared-modules/modal/modal.service';
 
 @Component({
   selector: 'app-turnos-empleados',
@@ -6,19 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./turnos-empleados.component.css']
 })
 export class TurnosEmpleadosComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private servicioHttp: AppHttpService, public servicioModal: ModalService) { }
 
-  datos: any | undefined;
+  datosEmp: any | undefined;
 
-  columnas = [
-    {field:'NITCliente',header:'NIT de Cliente'},
-    {field:'NombreCliente',header:'Nombre de Cliente'},
-    {field:'Precio',header:'Total Bs.'},
-    {field:'Fecha',header:'Fecha'},
-    {field:'Estado',header:'Estado'}
+  columnasEmp = [
+    {field:'Nombre',header:'Nombre'},
+    {field:'ApellidoP',header:'ApellidoP'},
+    {field:'ApellidoM',header:'ApellidoM'},
+    {field:'CI',header:'CI'},
+    {field:'Celular',header:'Celular'},
+    {field:'FechaNacimineto',header:'Fecha de Nacimiento'},
+    {field:'Cargo',header:'Cargo'}
   ];
-  nombreBotones: string[] | undefined;
+  nombreBotonesEmp: string[] | undefined;
+
+
+  datosTurn: any | undefined;
+
+  columnasTurn = [
+    {field:'Nombre',header:'Nombre'},
+    {field:'HoraInicio',header:'Hora Inicio'},
+    {field:'HoraFin',header:'Hora Fin'}
+  ];
+
 
   ngOnInit(): void {
   }
@@ -26,4 +42,22 @@ export class TurnosEmpleadosComponent implements OnInit {
 
   }
 
+  funcionAbrirTurnos(){
+    this.servicioModal.abrir('modalProd-1');
+  }
+
+  funcionCancelarTurno(){
+    
+    this.servicioModal.cerrar('modalProd-1');
+    (<HTMLInputElement>document.getElementById('rTurnos')).checked= false;
+  }
+
+  funcionAbrirTurnosEmpleados(){
+    this.servicioModal.abrir('modalTurosEmpleados');
+  }
+
+  funcionCancelarTurnEmpleado(){
+    this.servicioModal.cerrar('modalTurosEmpleados');
+    (<HTMLInputElement>document.getElementById('rTEmpleados')).checked= false;
+  }
 }
