9c3289f1a5f896151d2a6feddb5b040198c03db9
Boris Pedraza <bpedrazaa@gmail.com>
2021-12-02 15:09:05 -0400

    BT-02Balance Archivo transacciones modificado

--- YourPizzaBros3/transaccion.js
+++ YourPizzaBros3/transaccion.js
@@ -10,31 +10,29 @@ const fnHerramientas = require("./herramientas");
 	"Cantidad": 3000
 } body 
  */
+
 async function crearTransaccion(body) {
-  body.Fecha = fnHerramientas.stringAFecha(body.Fecha);
-  console.log(body.Fecha);
-  return fnHerramientas.createDoc(body, "Transaccion");
+  body.Fecha = firebase.firestore.Timestamp.fromDate(
+    fnHerramientas.stringAFecha(body.Fecha)
+  );
+  return await fnHerramientas.createDoc(body, "Transaccion");
 }
-
 async function obtenerTransaccion(idTrans) {
-  return fnHerramientas.getDoc(idTrans, "Transaccion");
+  return await fnHerramientas.getDoc(idTrans, "Transaccion");
 }
-
 async function obtenerTransacciones() {
-  return fnHerramientas.getDocs("Transaccion");
+  return await fnHerramientas.getDocs("Transaccion");
 }
-
 async function actualizarTransaccion(idTrans, body) {
   if (body.hasOwnProperty("Fecha")) {
     body.Fecha = firebase.firestore.Timestamp.fromDate(
       fnHerramientas.stringAFecha(body.Fecha)
     );
   }
-  return fnHerramientas.updateDoc(idTrans, body, "Transaccion");
+  return await fnHerramientas.updateDoc(idTrans, body, "Transaccion");
 }
-
 async function eliminarTransaccion(idTrans) {
-  return fnHerramientas.deleteDoc(idTrans, "Transaccion");
+  return await fnHerramientas.deleteDoc(idTrans, "Transaccion");
 }
 
 async function obtenerBalance() {
